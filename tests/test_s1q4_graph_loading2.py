c349072421695c45049a770ebdd5e7a53441b92a
GaranceValentin <127338619+GaranceValentin@users.noreply.github.com>
2023-04-06 10:43:43 +0200

    Update test_s1q4_graph_loading2.py

--- tests/test_s1q4_graph_loading2.py
+++ tests/test_s1q4_graph_loading2.py
@@ -1,4 +1,3 @@
-# This will work if ran from the root folder.
 import sys 
 sys.path.append("delivery_network")
 
@@ -12,7 +11,8 @@ class Test_Dist(unittest.TestCase):
         g = graph_from_file("input/network.04.in")
         self.assertEqual(g.graph[1][0][2], 6)
         self.assertEqual(g.graph[1][1][2], 89)
-
+        g1=graph_from_file("input/network.01.in")
+        self.assertEqual(g1.graph[1][0][2], 1)
         
 """ca renvoie pas les chemins sous la bonne forme"""
 if __name__ == '__main__':
