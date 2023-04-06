eff45e0c6d99733886e0dddaedb209d5e8049719
vsmague <valentin.smague@ensae.fr>
2023-04-06 18:52:23 +0000

    giiii

--- delivery_network/graph.py
+++ delivery_network/graph.py
@@ -1,6 +1,5 @@
 from heapq import *
 from graphviz import Graph as gr
-import math
 
 class Graph:
     """
@@ -71,102 +70,6 @@ class Graph:
         self.graph[node1].append((node2, power_min, dist))
         self.graph[node2].append((node1, power_min, dist))
         self.nb_edges += 1
-        #parcours en largeur : utiliser pour le plus court chemin BFS
-        #parcous en profondeur : utiliser pour une recherche de chemin DFS=
-
-    def get_path_with_power(self, src, dest, power): #questions 3 et 5
-        list = self.connected_components()
-        i = 0
-        while i < len(list) and src not in list[i]:
-            i += 1
-        if i == len(list): 
-            return "pas de source"
-        if dest not in list[i]:
-            return "source et destination non connectées"
-        return self.dijkstra(src, dest, power)
-
-    def dijkstra(self, s, t, power): #dijkstra pour question 3
-        Vu = set()
-        d = {s: 0}
-        predecesseurs = {}
-        suivants = [(0, s)]  # Â tas de couples (d[x],x)
-        while suivants != []:
-            dx, x = heappop(suivants)
-            if x in Vu:
-                continue
-            Vu.add(x)
-            for y, p, w in self.graph[x]:
-                if y in Vu:
-                    continue
-                dy = dx + w
-                if (y not in d or d[y] > dy) and power >= p:
-                    d[y] = dy
-                    heappush(suivants, (dy, y))
-                    predecesseurs[y] = x
-        path = [t]
-        if t not in d : return "pas de chemin possible avec cette puissance"
-        x = t
-        while x != s:
-            x = predecesseurs[x]
-            path.insert(0, x)
-        return path
-
-    def dfs(self,node,visited_nodes): #question 2
-        """ connected_graph = {}for key, values in self.graph.items(): connected_graph[key]=[values[0]]
-        on crée un dictionnaire qui prend comme clé le noeud et qui ajoute en valeur seulement le noeud voisin
-        le noeud voisin est bien contenu dans values = (nodes2, power_min, dist) """
-        """liste de liste : on va faire un dict"""
-        component = [node]
-        voisins = [node]
-        visited_nodes[node] = True
-        while component != []:
-            node=component[0]
-            component=component[1:]
-            for neighbour in self.graph[node]:
-                neighbour=neighbour[0]
-                if not visited_nodes[neighbour]:
-                    visited_nodes[neighbour] = True
-                    component.append(neighbour)
-                    voisins.append(neighbour)
-        return voisins
-
-    def connected_components(self): #question 2
-        list_components = []
-        visited_nodes = {noeud: False for noeud in self.nodes}
-
-        """création d'un dictionnaire de noeuds avec false si non visité"""
-        for noeud in self.nodes:
-            if not visited_nodes[noeud]:
-                list_components.append(Graph.dfs(self,noeud,visited_nodes))
-        return list_components
-
-    def connected_components_set(self): #question 2
-        return set(map(frozenset, self.connected_components()))
-        print(set(map(frozenset, self.connected_components())))
-
-    def min_power(self, src, dest):
-        """
-        Should return path, min_power.
-        """
-        p = 0
-        while type(self.get_path_with_power(src, dest, p)) == str :
-            p += 1
-        return self.get_path_with_power(src, dest, p), p
-    #on suppose ici que la puissance est toujours un entier naturel
-
-    def min_power2(self,src,dest): #question 6 : recherche dichotomique de puissance minimale
-        pmin=0
-        pmax=0
-        for nodes in self.graph:
-            for voisins in self.graph[nodes]:
-                pmax=max(pmax,voisins[1])
-        while pmin+1<pmax:
-            p=(pmin+pmax)//2
-            if type(Graph.get_path_with_power(self,src,dest,p))==str:
-                pmin=p
-            else:
-                pmax=p
-        return pmax
 
     def graph_from_file(filename): #questions 1 et 4
         """
@@ -202,7 +105,116 @@ class Graph:
                 else:
                     raise Exception("Format incorrect")
         return g
+    
+    def dfs(self,node,visited_nodes): #question 2
+        """ connected_graph = {}for key, values in self.graph.items(): connected_graph[key]=[values[0]]
+        on crée un dictionnaire qui prend comme clé le noeud et qui ajoute en valeur seulement le noeud voisin
+        le noeud voisin est bien contenu dans values = (nodes2, power_min, dist) """
+        """liste de liste : on va faire un dict"""
+        component = [node]
+        voisins = [node]
+        visited_nodes[node] = True
+        while component != []:
+            node=component[0]
+            component=component[1:]
+            for neighbour in self.graph[node]:
+                neighbour=neighbour[0]
+                if not visited_nodes[neighbour]:
+                    visited_nodes[neighbour] = True
+                    component.append(neighbour)
+                    voisins.append(neighbour)
+        return voisins
 
+    def connected_components(self): #question 2
+        list_components = []
+        visited_nodes = {noeud: False for noeud in self.nodes}
+
+        """création d'un dictionnaire de noeuds avec false si non visité"""
+        for noeud in self.nodes:
+            if not visited_nodes[noeud]:
+                list_components.append(Graph.dfs(self,noeud,visited_nodes))
+        return list_components
+
+    def connected_components_set(self): #question 2
+        return set(map(frozenset, self.connected_components()))
+        print(set(map(frozenset, self.connected_components())))
+    
+    def get_path_with_power(self, src, dest, power): #questions 3 et 5
+        list = self.connected_components()
+        i = 0
+        while i < len(list) and src not in list[i]:
+            i += 1
+        if i == len(list): 
+            return "pas de source"
+        if dest not in list[i]:
+            return "source et destination non connectées"
+        return self.chemin(src, dest, power)
+
+    def chemin(self, s, t, power): #dijkstra pour question 3
+        Vu = set()
+        d = {s: 0}
+        predecesseurs = {}
+        suivants = [(0, s)]  # Â tas de couples (d[x],x)
+        while suivants != []:
+            dx, x = heappop(suivants)
+            if x in Vu:
+                continue
+            Vu.add(x)
+            for y, p, w in self.graph[x]:
+                if y in Vu:
+                    continue
+                dy = dx + w
+                if (y not in d or d[y] > dy) and power >= p:
+                    d[y] = dy
+                    heappush(suivants, (dy, y))
+                    predecesseurs[y] = x
+        path = [t]
+        if t not in d : return "pas de chemin possible avec cette puissance"
+        x = t
+        while x != s:
+            x = predecesseurs[x]
+            path.insert(0, x)
+        return path
+    
+    def min_power_naif(self, src, dest):
+        """
+        Should return path, min_power.
+        """
+        p = 0
+        while type(self.get_path_with_power(src, dest, p)) == str :
+            p += 1
+        return self.get_path_with_power(src, dest, p), p
+    #on suppose ici que la puissance est toujours un entier naturel
+
+    def min_power(self,src,dest): #question 6 : recherche dichotomique de puissance minimale
+        pmin=0
+        pmax=0
+        for nodes in self.graph:
+            for voisins in self.graph[nodes]:
+                pmax=max(pmax,voisins[1])
+        while pmin+1<pmax:
+            p=(pmin+pmax)//2
+            if type(Graph.get_path_with_power(self,src,dest,p))==str:
+                pmin=p
+            else:
+                pmax=p
+        return pmax
+    
+    def representation(self, nom): #question 7 : représentation visuelle des graphes
+        graphe = gr(format='png', engine="circo") 
+        key=self.graph.keys()
+        sauv=[]
+        for i in key: # on crée tous les sommets
+            print(i)
+            graphe.node(f"{i}",f"{i}")
+            for voisin in self.graph[i]:
+                if voisin[0] not in sauv:
+                    graphe.edge(f"{i}", f"{voisin[0]}", label=f"p={voisin[1]},\n d={voisin[2]}")
+            sauv.append(i)
+        graphe.render(f"{nom}.dot")
+        print(graphe)
+        return()
+    
     def makeset(pik,rank,node): #pour implémenter Kruskal : question 12
         pik[node]=node
         rank[node]=0
@@ -241,57 +253,7 @@ class Graph:
                 Graph.add_edge(X,edge[0],edge[1],edge[2])
                 Graph.union(pik,rank,edge[0],edge[1])
         return(X)
-
-    def min_power_ameliore2(self,src,dest): #question 14
-        list = self.connected_components()
-        X=Graph.kruskal(self)
-        def power(self,node1,node2):
-            voisins=self.graph[node1]
-            n=len(voisins)
-            k=0
-            while voisins[k][0]!=node2:
-                k+=1
-            return voisins[k][1]
-        def dijkstra_unique(self, s, t):
-            Vu = set()
-            predecesseurs = {}
-            suivants = [s]
-            while suivants != []:
-                x = heappop(suivants)
-                if x in Vu:
-                    continue
-                Vu.add(x)
-                for y, p, w in self.graph[x]:
-                    if y in Vu:
-                        continue
-                    else:
-                        heappush(suivants, y)
-                        predecesseurs[y] = x
-            path = [t]
-            x = t
-            p_min=0
-            while x != s:
-                p_min=max(p_min,power(self,x,predecesseurs[x]))
-                x = predecesseurs[x]
-                path.insert(0, x)
-            return path,p_min
-        return dijkstra_unique(X, src, dest)
-
-    def representation(self, nom): #question 7 : représentation visuelle des graphes
-        graphe = gr(format='png', engine="circo") 
-        key=self.graph.keys()
-        sauv=[]
-        for i in key: # on crée tous les sommets
-            print(i)
-            graphe.node(f"{i}",f"{i}")
-            for voisin in self.graph[i]:
-                if voisin[0] not in sauv:
-                    graphe.edge(f"{i}", f"{voisin[0]}", label=f"p={voisin[1]},\n d={voisin[2]}")
-            sauv.append(i)
-        graphe.render(f"{nom}.dot")
-        print(graphe)
-        return()
-
+    
     def pre_travail(self): #pour implémenter la question 14
         list = self.connected_components()
         X=Graph.kruskal(self)
